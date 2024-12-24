7e08582ba98fc702babc6eeeb7085f1ccfd8a65d
Koishi <csolxqt@foxmail.com>
2024-12-24 15:49:11 +0800

    提交到GitHub

---
+++ appBuild/win/Tools.bat
@@ -0,0 +1,35 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFDpneTe+FW+/B4UO5+L+7P6UnnkSW+4sRJzJ2YiDIekd1lfhZpk4xHlfi/ccHhJOQhmkZxw8oFJWuWuJJPuVsADvdnyG6UcjVWx3iAM=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJgZk4aHUrQXA==
+::ZQ05rAF9IBncCkqN+0xwdVsEAlHMbSXoZg==
+::ZQ05rAF9IAHYFVzEqQIEJxRYX2Q=
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDpneTe+GG6pDaET+NTP4OWLsXEcRPIDeobV5oCBL+wApED8cPY=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+app.bat
