770e6e36664a864364041d49e66d42003352df6e
Koishi <csolxqt@foxmail.com>
2025-04-11 12:43:32 +0800

    新增设置表格行号接口

---
+++ src/main/java/priv/koishi/tools/Bean/Indexable.java
@@ -0,0 +1,19 @@
+package priv.koishi.tools.Bean;
+
+/**
+ * 为列表数据设置序号
+ *
+ * @author KOISHI
+ * Date:2025-04-10
+ * Time:16:00
+ */
+public interface Indexable {
+
+    /**
+     * 为列表数据设置序号接口
+     *
+     * @param index 要设置的序号
+     */
+    void setIndex(int index);
+
+}
