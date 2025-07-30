2f48b0d2a9aab965c10103c62d7ff6368eac71fa
Koishi <csolxqt@foxmail.com>
2025-07-30 19:21:59 +0800

    更新服务端脚本

--- appBuilder/ToolsUpdate/index.js
+++ appBuilder/ToolsUpdate/index.js
@@ -35,23 +35,23 @@ exports.main = async (event) => {
     }
     // 阿里云全量更新文件存储地址
     const aliyunAppLinks = {
-        win: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/PMC/win/Perfect Mouse Control 3.1.1-win.zip',
-        mac: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/PMC/mac/Perfect Mouse Control 3.1.1-mac.zip'
+        win: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/Tools/win/Tools 2.4.0-win.zip',
+        mac: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/Tools/mac/Tools-2.4.0-mac.zip'
     };
     // 阿里云增量更新文件存储地址
     const aliyunLibLinks = {
-        win: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/PMC/win/lib-3.1.1-win.zip',
-        mac: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/PMC/mac/lib-3.1.1-mac.zip'
+        win: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/Tools/win/lib-2.4.0-win.zip',
+        mac: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/Tools/mac/lib-2.4.0-mac.zip'
     };
     // 支付宝云全量更新文件存储地址
     const alipayAppLinks = {
-        win: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/PMC/win/Perfect%20Mouse%20Control%203.1.1-win.zip',
-        mac: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/PMC/mac/Perfect%20Mouse%20Control%203.1.1-mac.zip'
+        win: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/Tools/win/Tools%202.4.0-win.zip',
+        mac: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/Tools/mac/Tools-2.4.0-mac.zip'
     };
     // 支付宝云增量更新文件存储地址
     const alipayLibLinks = {
-        win: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/PMC/win/lib-3.1.1-win.zip',
-        mac: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/PMC/mac/lib-3.1.1-mac.zip'
+        win: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/Tools/win/lib-2.4.0-win.zip',
+        mac: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/Tools/mac/lib-2.4.0-mac.zip'
     };
     let aliyunFileLink, alipayFileLink;
     if (fullUpdate) {
