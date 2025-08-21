a31a9bf72a12290f21ae154b326309a2b9f9163d
Koishi <csolxqt@foxmail.com>
2025-08-21 18:15:13 +0800

    优化Properties读取方法

--- src/main/java/priv/koishi/tools/Properties/CommonProperties.java
+++ src/main/java/priv/koishi/tools/Properties/CommonProperties.java
@@ -5,7 +5,6 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Objects;
 import java.util.Properties;
 
 /**
@@ -24,28 +23,28 @@ public class CommonProperties extends Properties {
      */
     @Override
     public synchronized void load(Reader reader) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(reader);
-        while (true) {
-            // 缓冲流以行读取数据
-            String line = bufferedReader.readLine();
-            if (Objects.isNull(line)) {
-                break;
-            }
-            // 注意: properties属性类文件存在第一个隐藏字符,需要删除掉，否则第一个数据以key查找不存在
-            if (line.startsWith("\uFEFF")) {
-                line = line.substring(1);
-            }
-            // 如果是#注释内容，则不做操作
-            if (!line.startsWith("#") && !line.isEmpty()) {
-                // 将读取的数据格式为’=‘分割,以key,Value方式存储properties属性类文件数据
-                String key = line.split("=")[0];
-                String value = line.split("=")[1];
-                // value为空则不处理转义符
-                if (StringUtils.isNotBlank(value)) {
-                    // 由于‘\’在Java中表示转义字符，需要将读取的路径进行转换为‘/’符号,这里“\\\\”代表一个‘\’
-                    value = value.replaceAll("\\\\", "/");
+        try (BufferedReader bufferedReader = new BufferedReader(reader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                // 注意: properties属性类文件存在第一个隐藏字符,需要删除掉，否则第一个数据以key查找不存在
+                if (line.startsWith("\uFEFF")) {
+                    line = line.substring(1);
                 }
-                put(key, value);
+                // 如果是#注释内容，则不做操作
+                if (!line.startsWith("#") && !line.isEmpty()) {
+                    // 限制分割次数避免多等号问题
+                    String[] parts = line.split("=", 2);
+                    if (parts.length == 2) {
+                        String key = parts[0].trim();
+                        String value = parts[1].trim();
+                        // value为空则不处理转义符
+                        if (StringUtils.isNotBlank(value)) {
+                            // 由于‘\’在Java中表示转义字符，需要将读取的路径进行转换为‘/’符号,这里“\\\\”代表一个‘\’
+                            value = value.replaceAll("\\\\", "/");
+                        }
+                        put(key, value);
+                    }
+                }
             }
         }
     }
