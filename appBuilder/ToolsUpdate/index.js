e4f6cf6bd6d4856df2aabf40e4e4d0e8e3cd7be1
Koishi <csolxqt@foxmail.com>
2025-08-25 19:09:02 +0800

    更新服务端脚本

--- appBuilder/ToolsUpdate/index.js
+++ appBuilder/ToolsUpdate/index.js
@@ -16,11 +16,12 @@ exports.main = async (event) => {
     // 客户端当前版本
     const clientVersion = requestBody.version;
     // 服务端最新版本
-    const serverVersion = "2.5.7";
-    const buildDate = "2025.08.18";
+    const serverVersion = "2.6.0";
+    const buildDate = "2025.08.25";
     const features = [
-        "分组统计文件夹下文件数量功能合并到将图片与 excel 匹配并插入功能中",
-        "批量重命名功能的根据 excel 重命名文件功能支持读取 xls 文件",
+        "新增文件复制工具，可以按照设置项批量复制多个文件并重命名",
+        "文件移动工具新增取消移动功能",
+        "优化了自动操作工具的导入和导出功能",
         "调整了一些 UI 界面",
         "修复了一些 bug"
     ];
@@ -42,23 +43,23 @@ exports.main = async (event) => {
     }
     // 阿里云全量更新文件存储地址
     const aliyunAppLinks = {
-        win: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/Tools/win/Tools-2.5.7-win.zip',
-        mac: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/Tools/mac/Tools-2.5.7-mac.zip'
+        win: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/Tools/win/Tools-2.6.0-win.zip',
+        mac: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/Tools/mac/Tools-2.6.0-mac.zip'
     };
     // 阿里云增量更新文件存储地址
     const aliyunLibLinks = {
-        win: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/Tools/win/lib-2.5.7-win.zip',
-        mac: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/Tools/mac/lib-2.5.7-mac.zip'
+        win: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/Tools/win/lib-2.6.0-win.zip',
+        mac: 'https://mp-f42cc448-2bf2-4edf-9bb8-8f060ec60dd6.cdn.bspapp.com/Tools/mac/lib-2.6.0-mac.zip'
     };
     // 支付宝云全量更新文件存储地址
     const alipayAppLinks = {
-        win: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/Tools/win/Tools-2.5.7-win.zip',
-        mac: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/Tools/mac/Tools-2.5.7-mac.zip'
+        win: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/Tools/win/Tools-2.6.0-win.zip',
+        mac: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/Tools/mac/Tools-2.6.0-mac.zip'
     };
     // 支付宝云增量更新文件存储地址
     const alipayLibLinks = {
-        win: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/Tools/win/lib-2.5.7-win.zip',
-        mac: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/Tools/mac/lib-2.5.7-mac.zip'
+        win: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/Tools/win/lib-2.6.0-win.zip',
+        mac: 'https://env-00jxtp3qdq80.normal.cloudstatic.cn/Tools/mac/lib-2.6.0-mac.zip'
     };
     let aliyunFileLink, alipayFileLink;
     if (fullUpdate) {
